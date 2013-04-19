bcd015dd6b1d2d394defcafd8318a7a4876a7454
Dale Gartman <dgartman@gmu.edu>
2013-04-18 21:51:10 -0400

    ComboState now returns lists of frames

--- combostate.py
+++ combostate.py
@@ -1,12 +1,19 @@
+import os, pygame, json, random, csv, math
+from pygame.locals import *
+from utils import *
+
 def ComboState():
-	def __init__(self, main_sheet, spritesheets, dims, ):
+	def __init__(self, main_sheet, spritesheets, dims):
 		''' main_sheet should be a filename for the default sprite sheet
 			spritesheets should give a list of spritesheet image tuples
 			("name", "filename.jpg") ex. None should be duplicate names.
-			dims should be an (x, y, k) tuple which gives the width, height,
+			dims should be an (x, y, k) tuple which gives the width, height
 			and number of sprite images in the sprite sheet. '''
-		self.sheets = spritesheets
-		self.cur_sheet = main_sheet
+		self.sheet_map = []
+		for (name, filename) in spritesheets:
+			self.sheet_map.append((name,(extract_frames_from_spritesheet(filename, dims[0], dims[1], dims[2])))
+			
+		self.cur_sheet = extract_frames_from_spritesheet(main_sheet, dims[0], dims[1], dims[2])
 		self.def_sheet = main_sheet
 		self.combo_no = 0
 		
@@ -28,6 +35,6 @@ def ComboState():
 		return self.cur_sheet
 	
 	def find_state(self, sheetname):
-		for (name, filename) in self.spritesheets:
-			if name is sheetname: return filename
+		for (name, sheet) in self.spritesheets:
+			if name is sheetname: return sheetlist
 		
